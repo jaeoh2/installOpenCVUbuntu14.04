b6c308ef1d22ba8830e8b664824a4a4b61ac9581
Jaeoh2 <suby38@gmail.com>
2017-01-17 08:43:25 +0900

    Update cloneOpenCV.sh
    Update change the path

--- cloneOpenCV.sh
+++ cloneOpenCV.sh
@@ -5,6 +5,9 @@ cd $HOME
 mkdir tmp
 cd tmp
 git clone https://github.com/Itseez/opencv.git
+cd opencv
 git checkout 3.1.0
+cd ..
 git clone https://github.com/Itseez/opencv_contrib.git
+cd opencv_contrib
 git checkout 3.1.0
