b4d4ffd4d4728756ea03bcd7939ac7923a66e6dc
Jaeoh2 <suby38@gmail.com>
2017-01-17 11:51:06 +0900

    Update cloneOpenCV.sh
    Change git checkout release version to fix iss #1

--- cloneOpenCV.sh
+++ cloneOpenCV.sh
@@ -6,8 +6,8 @@ mkdir tmp
 cd tmp
 git clone https://github.com/Itseez/opencv.git
 cd opencv
-git checkout 3.1.0
+git checkout 3.2.0
 cd ..
 git clone https://github.com/Itseez/opencv_contrib.git
 cd opencv_contrib
-git checkout 3.1.0
+git checkout 3.2.0
