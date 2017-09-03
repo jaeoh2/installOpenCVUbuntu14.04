5d7872bf0c1d505c3cd6b2c88242774fc9257607
Jaeoh Lee <suby38@gmail.com>
2017-09-04 08:26:27 +0900

    Update installDependencies.sh

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Install Dependencies
+sudo apt-get update
 sudo apt-get install build-essential cmake git pkg-config -y
-sudo apt-get install libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev -y
-sudo apt-get install libgtk2.0-dev -y
+sudo apt-get install libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev -y
+sudo apt-get install qtbase5-dev libgtk2.0-dev -y
 sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
-sudo apt-get install libatlas-base-dev gfortran -y
+sudo apt-get install libatlas-base-dev libhdf5-serial-dev gfortran -y
 
 # Optional
 #sudo apt-add-repository ppa:mc3man/trusty-media
